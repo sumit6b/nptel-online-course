c8355ed16d6b5165fd37af83d39457ce2e5eb705
Sumit Kushwaha <su.mit.6b@gmail.com>
2014-05-04 14:46:05 +0530

    updated

--- Week7/7-1.c
+++ Week7/7-1.c
@@ -1,42 +1,16 @@
-#include<stdio.h>
-
-#include<stdlib.h>
-
-struct Event
-
-{
-
-   int start_time,finish_time;
-
-};
-
-int comp (const void * elem1, const void * elem2)
-
-{
-
-struct Event  f = *((struct Event*)elem1);
-
-struct Event  s = *((struct Event*)elem2);
-
-if (f.finish_time > s.finish_time) return  1;
-
-if (f.finish_time < s.finish_time) return -1;
-
-return 0;
-
-}
-
 int printMaxActivities(struct Event events[], int n)
+
 {
-    int counter=0,i,val,a,b;
+
+// insert code here
+    int counter=0,i,val;
     qsort(events,n,sizeof(struct Event),comp);
 
     for(i=0;i<n;i++)
     {
         if(i==0)
         {
-            a = events[i].start_time;
-            b = events[i].finish_time;
+            
             val = events[i].finish_time;
             counter++;
             continue;
@@ -47,12 +21,12 @@ int printMaxActivities(struct Event events[], int n)
             counter++;
         }
     }
-    return counter;
+    return counter;  
 }
 
 /**THE FOLLOWING CODE WILL BE APPENDED TO YOUR CODE BEFORE COMPILATION**/
 /**Do not uncomment this code. You should not write main()*/
-
+/*
 int main()
 
 {
@@ -71,9 +45,6 @@ int main()
 
        scanf("%d",&events[i].finish_time);
 
-
-
-
    count = printMaxActivities(events,n);
 
    printf("%d",count);
@@ -82,4 +53,4 @@ int main()
 
 }
 
-
+*/
